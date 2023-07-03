0edeeba50821b0876df93d8046fd80c609f7e4ed
Jonathon Stillson <jcstillson77@gmail.com>
2023-07-03 10:06:09 -0400

    added lang back

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './index.scss';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* GLOBAL VARIABLES */
 
-// window.$primaryLanguage = 'en';
-// window.$secondaryLanguage = 'pl';
-// window.$primaryLanguageIconId = 'primary-lang-icon';
-// window.$secondaryLanguageIconId = 'secondary-lang-icon';
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-// serviceWorker.register();
+serviceWorker.register();
